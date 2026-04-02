199f24ac8c02c8f4f7425ab40415915c4f178c97
Aasim <royalaasim@gmmail.com>
2026-04-02 18:01:12 +0530

    Added someuseful stuff

--- useful_code.py
+++ useful_code.py
@@ -1,4 +1,5 @@
 # reverse a word
+
 # def reverse_word():
 #   w = input("Enter a word: ")
 #   l = len(w)
@@ -6,7 +7,9 @@
 # # reverse_word()
 # print('------------------------')
 
+
 # find the product without using '*'
+
 # n1 = int(input("Enter the greater number: "))
 # n2 = int(input("Enter the smaller number: "))
 # i = 1
